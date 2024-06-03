a7527c7f433033dccbf247dd96659fd8b359967b
Alexandru <alexandrufiodor95@gmail.com>
2024-06-03 19:53:55 +0300

    homework 1

--- home-work/dist/index.js
+++ home-work/dist/index.js
@@ -3,16 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
 const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-const port = process.env.PORT || 8080;
-app.get('/', (_req, res) => {
-    return res.send('Express Typescript on Vercel');
-});
-app.get('/ping', (_req, res) => {
-    return res.send('pong 🏓');
-});
-app.listen(port, () => {
+const videos_routes_1 = require("./routes/videos-routes");
+exports.app = (0, express_1.default)();
+const port = 3000;
+const jsonBodyMiddleware = express_1.default.json();
+exports.app.use(jsonBodyMiddleware);
+exports.app.use('/videos', videos_routes_1.videosRoutes);
+exports.app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
 });
 //# sourceMappingURL=index.js.map
