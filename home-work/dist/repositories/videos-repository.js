6d626e789950053a6365bc82f5ea673abca9a98a
Alexandru <alexandrufiodor95@gmail.com>
2024-06-05 13:01:15 +0300

    some fixes

--- home-work/dist/repositories/videos-repository.js
+++ home-work/dist/repositories/videos-repository.js
@@ -6,39 +6,48 @@ exports.videosRepository = {
     findAllVideos() {
         return videosDB;
     },
-    findVideosById(id) {
-        return videosDB.find(video => video.id === id);
-    },
-    createVideo(title, author, availableResolutions) {
-        var _a;
-        const newVideo = {
-            id: ((_a = videosDB === null || videosDB === void 0 ? void 0 : videosDB[(videosDB === null || videosDB === void 0 ? void 0 : videosDB.length) - 1]) === null || _a === void 0 ? void 0 : _a.id) + 1 || 1,
-            title,
-            author,
-            canBeDownloaded: false,
-            minAgeRestriction: null,
-            createdAt: (new Date()).toISOString(),
-            publicationDate: (new Date()).toISOString(),
-            availableResolutions,
-        };
-        videosDB = [...videosDB, newVideo];
-        return newVideo;
-    },
-    updateVideo(id, video) {
-        const index = videosDB.findIndex(video => video.id === id);
-        if (index !== -1) {
-            videosDB[index] = Object.assign(Object.assign(Object.assign({}, videosDB[index]), video), { publicationDate: (new Date()).toISOString() });
-        }
-        return videosDB;
-    },
-    deleteVideo(id) {
-        for (let i = 0; i < videosDB.length; i++) {
-            if (videosDB[i].id === id) {
-                videosDB.splice(i, 1);
-                return true;
-            }
-        }
-        return false;
-    }
+    // findVideosById(id: number) {
+    //   return videosDB.find(video => video.id === id);
+    // },
+    // createVideo(title: string, author: string, availableResolutions: availableResolutions) {
+    //   const newVideo: video = {
+    //     id: videosDB?.[videosDB?.length -1]?.id + 1 || 1,
+    //     title,
+    //     author,
+    //     canBeDownloaded: false,
+    //     minAgeRestriction: null,
+    //     createdAt: (new Date()).toISOString(),
+    //     publicationDate: (new Date()).toISOString(),
+    //     availableResolutions,
+    //   }
+    //   videosDB = [...videosDB, newVideo]
+    //   return newVideo;
+    // },
+    // updateVideo(id: number, video: {
+    //   title: string,
+    //   author: string,
+    //   availableResolutions: availableResolutions,
+    //   canBeDownloaded: boolean,
+    //   minAgeRestriction: null | number,
+    // }) {
+    //   const index = videosDB.findIndex(video => video.id === id);
+    //   if (index !== -1) {
+    //     videosDB[index] = {
+    //       ...videosDB[index],
+    //       ...video,
+    //       publicationDate: (new Date()).toISOString(),
+    //     };
+    //   }
+    //   return videosDB;
+    // },
+    // deleteVideo(id: number) {
+    //   for (let i = 0; i < videosDB.length; i++) {
+    //     if (videosDB[i].id === id) {
+    //       videosDB.splice(i, 1);
+    //       return true;
+    //     }
+    //   }
+    //   return false;
+    // }
 };
 //# sourceMappingURL=videos-repository.js.map
