aab8fe8c695a4b38116701b6818dbaaeed5e6f6f
Alexandru <alexandrufiodor95@gmail.com>
2024-06-05 13:04:28 +0300

    some fixes

--- home-work/dist/index.js
+++ home-work/dist/index.js
@@ -5,12 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
-const videos_routes_1 = require("./routes/videos-routes");
+// import { videosRoutes } from './routes/videos-routes';
 exports.app = (0, express_1.default)();
 const port = 3000;
 const jsonBodyMiddleware = express_1.default.json();
 exports.app.use(jsonBodyMiddleware);
-exports.app.use('/videos', videos_routes_1.videosRoutes);
+let db = [];
+exports.app.get('/videos', (req, res) => {
+    res.json(db);
+});
+// app.use('/videos', videosRoutes)
 exports.app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
 });
--- home-work/src/index.ts
+++ home-work/src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { videosRoutes } from './routes/videos-routes';
+// import { videosRoutes } from './routes/videos-routes';
 
 export const app = express()
 const port = 3000
@@ -7,7 +7,11 @@ const port = 3000
 const jsonBodyMiddleware = express.json();
 app.use(jsonBodyMiddleware);
 
-app.use('/videos', videosRoutes)
+let db: any = []
+app.get('/videos', (req: any, res: any) => {
+  res.json(db);
+})
+// app.use('/videos', videosRoutes)
 
 app.listen(port, () => {
   return console.log(`Server is listening on ${port}`)
