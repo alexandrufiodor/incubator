2c0050e3dc203f8b92117aa30dfac8924c1ce329
Alexandru <alexandrufiodor95@gmail.com>
2024-06-06 13:00:26 +0300

    some fixes

--- home-work/src/middlewares/middlewares.ts
+++ home-work/src/middlewares/middlewares.ts
@@ -17,7 +17,7 @@ export const inputValidationMiddleware = (req: Request, res: Response, next: Nex
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const auth = { login: 'admin', password: 'qwerty' }
-  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const b64auth = (req.headers.authorization || '').split('Basic ')[1] || ''
   const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
   if (login && password && login === auth.login && password === auth.password) {
     return next()
