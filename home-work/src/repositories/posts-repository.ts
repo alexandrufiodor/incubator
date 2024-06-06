a3e7ca2049a2fc6962030febb096b835de494945
Alexandru <alexandrufiodor95@gmail.com>
2024-06-06 13:19:19 +0300

    add post api

--- home-work/src/repositories/posts-repository.ts
+++ home-work/src/repositories/posts-repository.ts
@@ -1,3 +1,5 @@
+import { blogsDB, blogsRepository } from './blogs-repository';
+
 type post = {
   id: string,
   title: string,
@@ -10,6 +12,54 @@ type post = {
 export let postsDB: Array<post> = []
 
 export const postsRepository = {
+  findAllPosts() {
+    return postsDB;
+  },
+  findPostById(id: string) {
+    return postsDB.find(post => post.id === id);
+  },
+  createPost(title: string, shortDescription: string, content: string, blogId: string) {
+    const findBlog = blogsRepository.findBlogById(blogId)
+    if (findBlog) {
+      const newPost: post = {
+        id: (new Date()).toISOString(),
+        title,
+        shortDescription,
+        content,
+        blogId,
+        blogName: findBlog?.name
+      }
+      postsDB = [...postsDB, newPost]
+      return newPost;
+    }
+    return false
+  },
+  updatePost(id: string, post: {
+    title: string, shortDescription: string, content: string, blogId: string
+  }) {
+    const findBlog = blogsRepository.findBlogById(post?.blogId)
+    if (findBlog) {
+      const index = postsDB.findIndex(post => post.id === id);
+      if (index !== -1) {
+        postsDB[index] = {
+          ...postsDB[index],
+          ...post,
+          blogId: findBlog?.name
+        };
+      }
+      return postsDB;
+    }
+    return false
+  },
+  deletePost(id: string) {
+    for (let i = 0; i < postsDB.length; i++) {
+      if (postsDB[i].id === id) {
+        postsDB.splice(i, 1);
+        return true;
+      }
+    }
+    return false;
+  },
   deleteAllPosts() {
     postsDB = [];
     return true;
--- home-work/src/routes/posts-routes.ts
+++ home-work/src/routes/posts-routes.ts
@@ -1,3 +1,81 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
+import { authMiddleware, inputValidationMiddleware } from '../middlewares/middlewares';
+import { postsRepository } from '../repositories/posts-repository';
 
 export const postsRoutes = Router();
+
+const titleValidation = body('title')
+  .notEmpty()
+  .withMessage('Title field is required.')
+  .isString()
+  .trim()
+  .isLength({min: 1, max: 30 })
+  .withMessage('Title length should be from 0 to 30 symbols');
+const shortDescriptionValidation = body('shortDescription')
+  .notEmpty()
+  .withMessage('shortDescription field is required.')
+  .isString()
+  .trim()
+  .isLength({min: 1, max: 100 })
+  .withMessage('shortDescription length should be from 0 to 100 symbols');
+const contentValidation = body('content')
+  .notEmpty()
+  .withMessage('content field is required.')
+  .isString()
+  .trim()
+  .isLength({min: 1, max: 1000 })
+  .withMessage('content length should be from 0 to 1000 symbols');
+const blogIdValidation = body('blogId')
+  .notEmpty()
+  .withMessage('blogId field is required.')
+  .isString()
+  .trim()
+  .withMessage('blogId length should be string');
+
+
+postsRoutes.get('/', (req, res) => {
+  res.send(postsRepository.findAllPosts())
+});
+postsRoutes.get('/:id', (req, res) => {
+  if (!req.params.id) {
+    res.sendStatus(404);
+  }
+  const findPost = postsRepository.findPostById(req.params.id);
+  if (!findPost) {
+    res.sendStatus(404);
+  }
+  res.send(findPost)
+})
+postsRoutes.post('/', authMiddleware, titleValidation, shortDescriptionValidation, contentValidation, blogIdValidation, inputValidationMiddleware, (req, res) => {
+  const createdPost = postsRepository.createPost(req.body?.title, req.body?.shortDescription, req.body?.content, req.body?.blogId)
+  if (createdPost) {
+    res.status(201).send(createdPost);
+  } else {
+    res.sendStatus(404)
+  }
+})
+postsRoutes.put('/:id', authMiddleware, titleValidation, shortDescriptionValidation, contentValidation, blogIdValidation, inputValidationMiddleware, (req, res) => {
+  if (!req.params.id) {
+    res.sendStatus(404);
+  }
+  const findPost = postsRepository.findPostById(req.params.id);
+  if (!findPost) {
+    res.sendStatus(404);
+  }
+  const updatedPost = postsRepository.updatePost(req.params.id, req.body)
+  if (updatedPost) {
+    res.sendStatus(204);
+  } else {
+    res.sendStatus(404);
+  }
+})
+postsRoutes.delete('/:id',authMiddleware, (req, res) => {
+  if (!req.params.id) {
+    res.sendStatus(404);
+  }
+  if (postsRepository.deletePost(req.params.id)) {
+    res.send(204);
+  }
+  res.sendStatus(404);
+})
