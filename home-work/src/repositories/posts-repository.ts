d641fefe872b136b07dbfaa0d0285d745198617e
Alexandru <alexandrufiodor95@gmail.com>
2024-06-06 13:40:37 +0300

    add post api

--- home-work/src/repositories/posts-repository.ts
+++ home-work/src/repositories/posts-repository.ts
@@ -43,7 +43,7 @@ export const postsRepository = {
         postsDB[index] = {
           ...postsDB[index],
           ...post,
-          blogId: findBlog?.name
+          blogName: findBlog?.name
         };
       }
       return postsDB;
